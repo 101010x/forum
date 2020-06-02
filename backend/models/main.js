fd4ad22339a8b7a75b94e79bfd3d1ab5e53337eb
mutugiii <mutugimutuma@gmail.com>
2020-06-02 15:36:44 +0300

    Write up models

--- backend/models/main.js
+++ backend/models/main.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const formSchema = mongoose.Schema({});
+const formSchema = mongoose.Schema({
+    question_number: { type: Number, required: true, unique: true }
+    question: { type: String, required: true },
+    answer: { type: String, required: false },
+    reason: { type: String, required: false },
+    reason_question: { type: String, required: false },
+    reason_answer: { type: String, required: false },
+});
 
 formSchema.plugin(uniqueValidator);
 
