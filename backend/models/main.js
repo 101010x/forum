ef1e7cad6979babeb3a043772e746816dfdec395
mutugiii <mutugimutuma@gmail.com>
2020-06-03 22:53:00 +0300

    Frontend Structure

--- backend/models/main.js
+++ backend/models/main.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const formSchema = mongoose.Schema({
-    question_number: { type: Number, required: true, unique: true },
+    section_number: { type: Number, required: true }
+    question_number: { type: Number, required: true },
     question: { type: String, required: true },
     answer: { type: String, required: false },
     reason: { type: String, required: false },
